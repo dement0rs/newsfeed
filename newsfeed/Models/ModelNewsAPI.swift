4850e9a49e2b10b84e8b1d5d6c2fbc38b3f0bc04
Anna Oksanichenko <oksanichenkoann@gmail.com>
2021-02-08 13:51:46 +0200

    started working on models for api

---
+++ newsfeed/Models/ModelNewsAPI.swift
@@ -0,0 +1,37 @@
+//
+//  ModelNewsAPI.swift
+//  newsfeed
+//
+//  Created by Anna Oksanichenko on 08.02.2021.
+//
+
+import Foundation
+
+// MARK: - ModelForNews
+struct ModelForNews: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source
+    let author, title, articleDescription: String
+    let url: String
+    let urlToImage: String
+    let publishedAt: Date
+    let content: String
+
+    enum CodingKeys: String, CodingKey {
+        case source, author, title
+        case articleDescription = "description"
+        case url, urlToImage, publishedAt, content
+    }
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: String?
+    let name: String
+}
